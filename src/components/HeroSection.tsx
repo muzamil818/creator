8845fafb2c7f4105e21f6e374e071223dd30bc3b
muzamil al <muzamilali.dev@gmail.com>
2025-09-09 23:53:05 +0500

    added left section to the hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,18 +1,28 @@
 import leftHand from "../assets/leftHand.png";
 import leftLine from "../assets/leftLine.png";
+import instagram from "../assets/insta.png";
+import facebook from "../assets/facebook.png";
+import bulb from "../assets/bulb.png";
 const HeroSection = () => {
   return (
     <div className="w-full h-[80vh]">
       {/* left section */}
-      <div className="relative">
-        <div className="z-5">
-          <img src={leftHand} alt="" />
-        </div>
-        <img className="absolute top-0 z-3" src={leftLine} alt="" />
+      <div className="relative h-[127vh] w-1/4">
+        <img
+          src={leftLine}
+          alt=""
+          className="absolute top-0 left-3 z-0 "
+        />
+        <img src={leftHand} alt="" className="relative z-10" />
+        <img className="absolute top-82 left-13" src={instagram} alt="" />
+        <img className="absolute bottom-0 left-30" src={facebook} alt="" />
+        <img className="absolute top-59 right-0  h-18" src={bulb} alt="" />
       </div>
 
       {/* Middle section */}
-      <div></div>
+      <div>
+        <h1></h1>
+      </div>
 
       {/* Right section */}
       <div></div>
